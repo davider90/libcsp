a370edf886e0903d70cb5f7b972c2d75c79cd725
perwulff <pwu@gomspace.com>
2019-09-06 11:45:44 +0200

    Added integration of GomSpace log system.

---
+++ include/csp/external/csp_debug.h
@@ -0,0 +1,29 @@
+/* Copyright (c) 2013-2019 GomSpace A/S. All rights reserved. */
+#ifndef GS_CSP_EXTERNAL_CSP_DEBUG_H
+#define GS_CSP_EXTERNAL_CSP_DEBUG_H
+
+#include <gs/util/log/log.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+GS_LOG_GROUP_EXTERN(gs_csp_log);
+
+#define csp_log_error(...)     log_error_group(gs_csp_log, ##__VA_ARGS__)
+#define csp_log_warn(...)      log_warning_group(gs_csp_log, ##__VA_ARGS__)
+#define csp_log_info(...)      log_info_group(gs_csp_log, ##__VA_ARGS__)
+
+#define csp_log_packet(...)    log_info_group(gs_csp_log, ##__VA_ARGS__)
+#define csp_log_buffer(...)    log_trace_group(gs_csp_log, ##__VA_ARGS__)
+#define csp_log_protocol(...)  log_debug_group(gs_csp_log, ##__VA_ARGS__)
+#define csp_log_lock(...)      log_trace_group(gs_csp_log, ##__VA_ARGS__)
+
+#define csp_debug_toggle_level(...)  {}
+#define csp_debug_set_level(...)     {}
+#define csp_debug_get_level(...)     {}
+    
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ src/external/csp_debug.c
@@ -0,0 +1,11 @@
+/* Copyright (c) 2013-2019 GomSpace A/S. All rights reserved. */
+
+/**
+   @file
+
+   @note THIS FILE IS COMPILED AS PART OF libcsp.
+*/
+
+#include <csp/external/csp_debug.h>
+
+GS_LOG_GROUP(gs_csp_log, "csp", GS_LOG_CAT_CSP, LOG_DEFAULT_MASK);
