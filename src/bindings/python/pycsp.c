657eaa06b9cac7696b8476984009957454022e9d
perwulff <pwu@gomspace.com>
2019-10-24 14:11:44 +0200

    Fixed Python bindings.

--- src/bindings/python/pycsp.c
+++ src/bindings/python/pycsp.c
@@ -756,12 +756,15 @@ static PyObject* pycsp_can_socketcan_init(PyObject *self, PyObject *args)
     int bitrate = 1000000;
     int promisc = 0;
 
-    if (!PyArg_ParseTuple(args, "s|ii", &ifc, &bitrate, &promisc))
-    {
+    if (!PyArg_ParseTuple(args, "s|ii", &ifc, &bitrate, &promisc)) {
         return NULL;
     }
 
-    csp_can_socketcan_init(ifc, CSP_IF_CAN_DEFAULT_NAME, bitrate, promisc);
+    int res = csp_can_socketcan_open_and_add_interface(ifc, CSP_IF_CAN_DEFAULT_NAME, bitrate, promisc, NULL);
+    if (res != CSP_ERR_NONE) {
+        return NULL;
+    }
+
     Py_RETURN_NONE;
 }
 
