f0de36fae52b9d9d4b2776ab88bef90fa2f2eaf2
perwulff <pwu@gomspace.com>
2020-01-20 17:34:31 +0100

    Updated from libcsp::libcsp.

--- src/arch/posix/csp_thread.c
+++ src/arch/posix/csp_thread.c
@@ -22,6 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include <limits.h>
 #include <unistd.h>
+#include <time.h>
 #include <errno.h>
 
 int csp_thread_create(csp_thread_func_t routine, const char * const thread_name, unsigned int stack_size, void * parameters, unsigned int priority, csp_thread_handle_t * return_handle) {
